6ac6ca89e83b8cd7f5e997ab96ba2caeab02b627
Husbando <103307099+Husbandoo@users.noreply.github.com>
2022-05-22 23:20:23 +0530

    Update hacking_script.py

--- NekoRobot/hacking_script.py
+++ NekoRobot/hacking_script.py
@@ -1,6 +1,6 @@
 PM_START_TEXT = """
 ────「 [{}] 」────
-*やあ! Hello {} My Darling, I'am your SweetHeart & Here To Protect You,
+*やあ! Hello {} My Darling ,
 I'm An Anime Themed Multi-Featured & Advanced Group Management Bot To Manage Your Group.*
 
 ◈ *Version :* `3.10.2`
