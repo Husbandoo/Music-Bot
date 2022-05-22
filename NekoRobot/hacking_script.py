55c10c7e012e1a31b66cebc9ead1a4be19332f82
Husbando <103307099+Husbandoo@users.noreply.github.com>
2022-05-22 14:43:05 +0530

    Update hacking_script.py

--- NekoRobot/hacking_script.py
+++ NekoRobot/hacking_script.py
@@ -13,3 +13,6 @@ I'm An Anime Themed Multi-Featured & Advanced Group Management Bot To Manage You
 
 PM_START_PIC = "https://te.legra.ph/file/9abdbb8e7fa75e03a91ea.jpg"
 
+
+## Don't Remove That Credits in PM_START_TEXT if yoy removed your app will be crashed.
+
